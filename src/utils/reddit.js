1005146d7870e88d6ffa0f8d6cee398e29d95776
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-03-01 19:36:43 +0200

    feat: archiving formats

--- src/utils/reddit.js
+++ src/utils/reddit.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-export async function fetchFeed(topics) {
+export async function fetchFeed(topics, type) {
   try {
+    let mimeType = "application/json";
+    let limit = 100;
+    let ArchiveType = 1;
+
+    if (type === "PRTSCR") {
+      mimeType = "image/png";
+      // safe lower limit because
+      // screenshots size is ~ 10x
+      // larger than storing metadata
+      limit = 25;
+      ArchiveType = 2;
+    }
     const feed = [];
 
     for (let topic of topics) {
-      const url = `https://www.reddit.com/search.json?q=${topic}&sort=new&limit=100`;
+      const url = `https://www.reddit.com/search.json?q=${topic}&sort=new&limit=${limit}`;
       const request = await axios.get(url);
 
       const childs = request.data.data.children;
@@ -13,7 +25,8 @@ export async function fetchFeed(topics) {
       for (const post of childs) {
         const child = post.data;
         const tags = {
-          "App-Name": "reddit-archiver",
+          "App-Name": "reddit-archiver-test",
+          "archive-type": ArchiveType,
           "archiver-topic": topic,
           subdreddit: child.subreddit,
           subreddit_id: child?.subreddit_id,
@@ -22,7 +35,7 @@ export async function fetchFeed(topics) {
           title: child?.title,
           url: child?.url,
           reddit_timestamp: child?.created_utc,
-          "Content-Type": "application/json",
+          "Content-Type": mimeType,
         };
 
         feed.push({ child, tags });
